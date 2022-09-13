1630925bf6b6747bd839ff2c4829c98027214dae
mariaIvanova-vn <maria_vn86@abv.bg>
2022-09-13 11:26:36 +0300

    change

--- 01.Syntax, Functions and Statements - Lab/01. Echo Function.js
+++ 01.Syntax, Functions and Statements - Lab/01. Echo Function.js
@@ -1,14 +1,6 @@
-function echoFunc(first,second, third){
-    let firstLength = first.length;
-    let secondLength = second.length;
-    let thirdLength = third.length;
-
-    let sum = firstLength + secondLength + thirdLength;
-
-    let average = Math.floor(sum / 3);
-
-    console.log(sum);
-    console.log(average);
+function echoFunc(input){
+    console.log(input.length);
+    console.log(input);
 }
 
-echoFunc('Maria', 'Richie', 'SoftUni');
+echoFunc('Hello, JavaScript!');
