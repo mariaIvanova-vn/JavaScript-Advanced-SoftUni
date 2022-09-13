cd365507f08bc268a64883771e2c9fac3f1fa210
mariaIvanova-vn <maria_vn86@abv.bg>
2022-09-13 11:22:55 +0300

    First Problem

---
+++ 01.Syntax, Functions and Statements - Lab/01. Echo Function.js
@@ -0,0 +1,14 @@
+function echoFunc(first,second, third){
+    let firstLength = first.length;
+    let secondLength = second.length;
+    let thirdLength = third.length;
+
+    let sum = firstLength + secondLength + thirdLength;
+
+    let average = Math.floor(sum / 3);
+
+    console.log(sum);
+    console.log(average);
+}
+
+echoFunc('Maria', 'Richie', 'SoftUni');
