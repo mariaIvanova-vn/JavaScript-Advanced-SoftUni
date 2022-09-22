0768e662ec32fce9a5b27141847aebfcd66e6ccc
mariaIvanova-vn <maria_vn86@abv.bg>
2022-09-22 15:51:25 +0300

    add

---
+++ 03.Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,11 @@
+function solve(name, population, treasury) {
+    return {
+        name,
+        population,
+        treasury
+    };
+}
+
+let result = solve('Tortuga', 7000, 15000);
+
+console.log(result)
---
+++ 03.Objects and Composition - Lab/02. Town Population.js
@@ -0,0 +1,9 @@
+function solve(array){
+
+}
+
+solve(['Sofia <-> 1200000',
+'Montana <-> 20000',
+'New York <-> 10000000',
+'Washington <-> 2345000',
+'Las Vegas <-> 1000000'])
