bb450908cab9aeb1384aa67d5de42c2b556a7748
mariaIvanova-vn <maria_vn86@abv.bg>
2022-09-23 13:25:23 +0300

    Completed Task 02.TownPopulation

--- 03.Objects and Composition - Lab/02. Town Population.js
+++ 03.Objects and Composition - Lab/02. Town Population.js
@@ -1,5 +1,23 @@
 function solve(array){
-
+let townData = array.map(el => {
+    let data = el.split(' <-> ');
+    return{
+        name: data[0],
+        population: Number(data[1])
+    };
+});
+let result = {};
+for(let town of townData){
+    if(result[town.name] === undefined){
+        result[town.name] = town.population;
+    }
+    else{
+        result[town.name] += town.population;
+    }
+}
+for(let town in result){
+    console.log(`${town} : ${result[town]}`);
+}
 }
 
 solve(['Sofia <-> 1200000',
