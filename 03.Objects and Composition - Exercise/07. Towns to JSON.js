f9da16d65e6028361203cf04f31c1884e91f9156
mariaIvanova-vn <maria_vn86@abv.bg>
2022-09-26 12:54:28 +0300

    add solutions

--- 03.Objects and Composition - Exercise/07. Towns to JSON.js
+++ 03.Objects and Composition - Exercise/07. Towns to JSON.js
@@ -1,8 +1,25 @@
-function solve() {
+function solve(arr) {
+    let result = [];
+    const properties = arr[0].split(/\s*\|\s*/mg).filter(e => e.trim());
+    for (let i = 1; i < arr.length; i++) {
+        let data = arr[i].split(/\s*\|\s*/mg).filter(e => e.trim());
 
+        let name = data[0];
+        let latitude = Number(data[1]).toFixed(2);
+        let longtitude = Number(data[2]).toFixed(2);
+
+        let obj = {};
+        obj[properties[0]] = name;
+        obj[properties[1]] = Number(latitude);
+        obj[properties[2]] = Number(longtitude);
+
+        result.push(obj);
+    }
+    let jsonString = JSON.stringify(result);
+    return jsonString;
 }
 
 
-solve(['| Town | Latitude | Longitude |',
+console.log(solve(['| Town | Latitude | Longitude |',
     '| Sofia | 42.696552 | 23.32601 |',
-    '| Beijing | 39.913818 | 116.363625 |'])
+    '| Beijing | 39.913818 | 116.363625 |']))
--- 03.Objects and Composition - Exercise/08. Rectangle.js
+++ 03.Objects and Composition - Exercise/08. Rectangle.js
@@ -1,5 +1,12 @@
-function rectangle(){
-
+function rectangle(width, height, color) {
+    return {
+        width,
+        height,
+        color: color[0].toUpperCase() + color.slice(1, color.length),
+        calcArea: function () {
+            return width * height;
+        }
+    }
 }
 
 
