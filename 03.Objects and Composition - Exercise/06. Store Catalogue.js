cc4c353819238d15e5077235d46445a482f4182f
mariaIvanova-vn <maria_vn86@abv.bg>
2022-09-26 11:15:53 +0300

    Completed task 06.Store Catalogue

--- 03.Objects and Composition - Exercise/06. Store Catalogue.js
+++ 03.Objects and Composition - Exercise/06. Store Catalogue.js
@@ -1,5 +1,25 @@
-function storeCatalogue() {
+function storeCatalogue(arr) {
+    let productCatalogue = {};
 
+    arr.forEach(element => {
+        let [productName, price] = element.split(" : ");
+        price = Number(price);
+        let index = productName[0];
+        if (!productCatalogue[index]) {
+            productCatalogue[index] = {};
+        }
+        productCatalogue[index][productName] = price;
+    });
+    let initialSort = Object.keys(productCatalogue).sort((a, b) => a.localeCompare(b));
+
+    for (let key of initialSort) {
+        let products = Object.entries(productCatalogue[key]).sort((a, b) => a[0].localeCompare(b[0]));
+
+        console.log(key);
+        products.forEach((element) => {
+            console.log(`  ${element[0]}: ${element[1]}`)
+        })
+    }
 }
 
 
