23df521b0606a38a63982182845703e4260b15a9
mariaIvanova-vn <maria_vn86@abv.bg>
2022-09-29 12:24:17 +0300

    add solutions

--- 04.DOM Introduction - Exercise/07. Hell's Kitchen/app.js
+++ 04.DOM Introduction - Exercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,48 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+      let input = JSON.parse(document.querySelector('#inputs textarea').value);
+      let allRestaurants = {};
+      let bestAvgSalary = 0;
+      let bestRestaurantName = '';
+
+      for (let data of input) {
+         let [restaurantName, workers] = data.split(' - ');
+         let worker = workers.split(', ');
+
+         for (let w of worker) {
+            let [name, salary] = w.split(' ');
+
+            if (!allRestaurants.hasOwnProperty(restaurantName)) {
+               allRestaurants[restaurantName] = {};
+            }
+            allRestaurants[restaurantName][name] = Number(salary);
+            console.log(allRestaurants);
+         }
+         let entries = Object.entries(allRestaurants);
+
+         for (let [name, workers] of entries) {
+            let salaries = Object.values(workers);
+            let totalSalariesPaid = 0;
+
+            for (let salary of salaries) {
+               totalSalariesPaid += salary;
+            }
+
+            let averageSalary = totalSalariesPaid / salaries.length;
+
+            if (averageSalary > bestAvgSalary) {
+               bestAvgSalary = averageSalary;
+               bestRestaurantName = name;
+            }
+         }
+      }
+      let workersOrdered = Object.entries(allRestaurants[bestRestaurantName]).sort((a, b) => b[1] - a[1]);
+      let workersAsString = '';
+      workersOrdered.forEach(w => workersAsString += `Name: ${w[0]} With Salary: ${w[1]} `);
+
+      document.querySelector('#bestRestaurant p').textContent = `Name: ${bestRestaurantName} Average Salary: ${bestAverageSalary.toFixed(2)} Best Salary: ${workersOrdered[0][1].toFixed(2)}`;
+      document.querySelector('#workers p').textContent = workersAsString;
    }
 }
--- 04.DOM Introduction - Exercise/08. Generate Report/report.js
+++ 04.DOM Introduction - Exercise/08. Generate Report/report.js
@@ -1,3 +1,36 @@
 function generateReport() {
-    //TODO
+    let checkedIndexes = [];
+    let checkedBoxes = document.querySelectorAll("input");
+    let checkedBoxesArr = Array.from(checkedBoxes);
+
+    checkedBoxesArr.forEach((el, index) => {
+        if (el.checked) {
+            checkedIndexes.push(index);
+        }
+    });
+
+    for (let i = 0; i < checkedBoxes.length; i++) {
+        let el = checkedBoxes[i];
+        if (el.checked) {
+            checkedIndexes.push(i);
+        }
+    }
+
+    let result = [];
+    let rowses = document.querySelectorAll("tbody tr");
+    let rows = Array.from(rowses);
+
+    for (let row of rows) {
+        let obj = {};
+
+        for (let index of checkedIndexes) {
+            let name = checkedBoxes[index].name;
+            let info = row.children[index].textContent;
+            obj[name] = info;
+        }
+        result.push(obj);
+    }
+    let output = JSON.stringify(result);
+    let outputEl = document.getElementById("output");
+    outputEl.textContent = output;
 }
