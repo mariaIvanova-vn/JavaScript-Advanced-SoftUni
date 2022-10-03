2644469b1585e4042617465d3c4c7852eaa603e4
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-03 15:15:04 +0300

    add

--- 05.DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
+++ 05.DOM Manipulation and Events - Lab/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
     console.log('TODO:...');
+    let list=document.getElementById('items');
+
+    let input = document.getElementById('newItemText');
+    let newItemValue = input.value;
+    let liItem = document.createElement('li');
+    liItem.textContent=newItemValue;
+    list.appendChild(liItem);
+    input.value='';
 }
