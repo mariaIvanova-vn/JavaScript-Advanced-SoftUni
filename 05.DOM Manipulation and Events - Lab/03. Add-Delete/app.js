8c5868c1cb79644488ee723b606ec6c2550c654e
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-05 10:38:47 +0300

    add 03. Add-Delete

--- 05.DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ 05.DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,20 @@
 function addItem() {
-    //TODO...
+    let list = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+
+    let listItem = document.createElement('li');
+    listItem.textContent = inputElement.value;
+
+    let linkElement = document.createElement('button');
+    linkElement.href = '#';
+    linkElement.textContent = '[Delete]';
+
+    listItem.appendChild(linkElement);
+    list.appendChild(listItem);
+
+    linkElement.addEventListener('click', () => {
+        linkElement.parentNode.remove();
+    });
+
+    inputElement.value = '';
 }
