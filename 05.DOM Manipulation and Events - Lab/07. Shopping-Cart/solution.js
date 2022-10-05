14bc8e18d3d82f9d3616d429e7ece1770ab5ef16
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-05 16:05:36 +0300

    Add solution for 07. Shopping-Cart

--- 05.DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ 05.DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,37 @@
 function solve() {
-   //TODO...
+   let shopingCard = document.getElementsByClassName('shopping-cart')[0];
+   let result = document.getElementsByTagName('textarea')[0];
+
+   let products = [];
+   let totalPrice = 0;
+   let checkOut = false;
+
+   shopingCard.addEventListener('click', function (event) {
+      if (event.target.nodeName !== 'BUTTON') {
+         return;
+      }
+     
+      let button = event.target;
+      if(checkOut ===true){
+         return;
+      }
+      if (Array.from(button.classList).indexOf('add-product') >= 0) {
+         let product = event.target.parentElement.parentElement;
+         console.log(product)
+         let name = product.querySelectorAll('.product-title')[0].textContent;
+         let price = product.querySelectorAll('.product-line-price')[0].textContent;
+
+         result.textContent += `Added ${name} for ${price} to the cart.\n`;
+
+         if (products.indexOf(name) < 0) {
+            products.push(name);
+         }
+         totalPrice += Number(price);
+      }
+      else if (Array.from(button.classList).indexOf('checkout') >= 0) {
+checkOut=true;
+let list=products.join(", ");
+result.textContent += `You bought ${list} for ${totalPrice.toFixed(2)}.`
+      }
+   })
 }
