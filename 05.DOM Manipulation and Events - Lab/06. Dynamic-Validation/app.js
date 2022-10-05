2c3967a03c8e6fa9ad9cccd5cb3cbdcd274e7203
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-05 11:16:22 +0300

    add changes

--- 05.DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ 05.DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    let inputEmail = document.getElementById('email');
+    let pattern = /[a-z]+@[a-z]+.[a-z]+/g;
+
+    let validate = function (event) {
+        if (pattern.test(event.currentTarget.value)) {
+            event.currentTarget.removeAttribute('class');
+        } else {
+            event.currentTarget.classList.add('error');
+        }
+    }
+    inputEmail.addEventListener('change', validate);
 }
