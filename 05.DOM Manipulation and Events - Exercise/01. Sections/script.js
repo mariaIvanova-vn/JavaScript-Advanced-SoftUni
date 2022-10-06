a7c5fb7840ed4c848e461ebf59fc23f6cc17f700
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-06 08:52:21 +0300

    add solution for 01. Sections

--- 05.DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ 05.DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
+   for (let w of words) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+
+      p.innerText = w;
+      p.style.display = 'none';
+
+      div.appendChild(p);
+      div.addEventListener('click', (event => {
+         if (event.target.nodeName === 'p') {
+            return;
+         }
+         let p = event.target.children[0];
+         p.style.display = 'block';
+      }));
+      document.getElementById('content').appendChild(div);
+   }
 }
