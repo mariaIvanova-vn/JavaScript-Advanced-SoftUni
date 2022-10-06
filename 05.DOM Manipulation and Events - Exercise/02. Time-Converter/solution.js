e5cfaade468fd308ef58bb445bfa3a6c1ace830f
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-06 10:05:42 +0300

    add 02. Time-Converter

--- 05.DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
+++ 05.DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,30 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let buttons = Array.from(document.querySelectorAll('input[type=button]'));
+
+    for (let button of buttons) {
+        button.addEventListener('click', (event => {
+            let value = Number(event.target.parentElement.querySelector('input[type=text]').value);
+            let btnType = event.target.id;
+
+            if (btnType === 'daysBtn') {
+                calculate(value);
+            } else if (btnType === 'hoursBtn') {
+                calculate(value / 24);
+            } else if (btnType === 'minutesBtn') {
+                calculate(value / 24 / 60);
+            } else if (btnType === 'secondsBtn') {
+                calculate(value / 24 / 60 / 60);
+            }
+        }))
+    }
+    function calculate(value) {
+        let inputs = Array.from(document.querySelectorAll('input[type=text]'));
+        inputs.shift().value = value;
+        let minutes = value * 24;
+        for (let input of inputs) {
+            input.value = minutes;
+            minutes *= 60;
+        }
+    }
 }
