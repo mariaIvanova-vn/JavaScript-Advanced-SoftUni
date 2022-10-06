adbf73a4f454aa8cea1df251bffe44a27c3ba8d8
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-06 10:53:26 +0300

    add

--- 05.DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ 05.DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let buttons = document.querySelectorAll('button');
+    buttons[0].addEventListener('click', (event) => {
+        let newMessage = '';
+        let currText = event.target.parentElement.children[1].value;
+
+        for (let i = 0; i < currText.length; i++) {
+            let currCh = currText[i].charCodeAt();
+            newMessage += String.fromCharCode(currCh + 1);
+        }
+        let text = document.querySelectorAll('textarea');
+        let resultText = text[1];
+        resultText.value = newMessage;
+        let currTextArea = text[0];
+        currTextArea.value = '';
+    });
+    buttons[1].addEventListener('click', (event) => {
+        let newMessage = '';
+        let currArea = event.target.parentElement.children[1];
+        let currText = currArea.value;
+
+        for (let i = 0; i < currText.length; i++) {
+            let currCh = currText[i].charCodeAt();
+            newMessage += String.fromCharCode(currCh - 1);
+        }
+        currArea.value=newMessage;
+    });
 }
