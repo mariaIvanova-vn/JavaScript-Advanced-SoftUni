50ef2c2772042a577bab21710f3a33a8c9f07486
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-06 15:44:19 +0300

    add

--- 05.DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ 05.DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+    let select = document.getElementById('menu');
+
+    let option = document.createElement('option');
+    option.textContent = newItemText.value;
+    option.value = newItemValue.value;
+
+    select.appendChild(option);
+
+    newItemText.value = '';
+    newItemValue.value = '';
 }
