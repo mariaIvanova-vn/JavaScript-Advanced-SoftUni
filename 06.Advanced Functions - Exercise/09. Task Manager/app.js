1e3fa10ef21472fd249785120b75e1566a117ebd
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-11 14:48:07 +0300

    add

--- 06.Advanced Functions - Exercise/09. Task Manager/app.js
+++ 06.Advanced Functions - Exercise/09. Task Manager/app.js
@@ -1,16 +1,88 @@
 function solve() {
-    document.getElementsByTagName('form')[0].addEventListener('submit', createTask);
-    let sections = document.getElementsByTagName('section');
-    let openSection = sections[1];
-    let inProgressSection = sections[2];
-    let completeSections = sections[3];
+    document.querySelector('#add').addEventListener('click', createTask);
+    let [addSection, openSection, inProgress, complete] = Array.from(document.getElementsByTagName('section'));
+    let buttons = {
+        open: { green: 'Start', red: 'Delete' },
+        inProgress: { red: 'Delete', orange: 'Finish' },
+    }
 
     function createTask(event) {
         event.preventDefault();
-        let form = event.target;
-        let task = form.elements[0].value;
-        let description = form.elements[1].value;
-        let date = form.elements[2].value;
-        debugger
+
+        let elements = ['h3', 'p', 'p'];
+        let form = event.target.parentNode;
+        let title = form.elements['task'].value;
+        let description = 'Description: ' + form.elements['description'].value
+        let date = 'Due Date: ' + form.elements['date'].value;
+
+        let data = [title, description, date];
+        if (data.includes('')) {
+            return;
+        }
+
+        let article = generateArticle(elements, data, buttons);
+        openSection.children[1].appendChild(article);
+
+        article.addEventListener('click', (ev) => {
+            let actions = eventDelegator(buttons);
+            let button = ev.target.textContent;
+            let buttonsDiv = ev.target.parentNode;
+            let article = ev.currentTarget;
+
+            if (ev.target.tagName != 'BUTTON') {
+                return;
+            }
+            if (button == 'Start') {
+                buttonsDiv.remove();
+                actions['start'](article);
+            } else if (button == 'Finish') {
+                buttonsDiv.remove();
+                actions['finish'](article);
+            } else if (button == 'Delete') {
+                actions['delete'](article)
+            }
+        });
+    }
+
+    function eventDelegator(buttons) {
+        let functions = {
+            start: (article) => {
+                article.appendChild(generateButtons(buttons['inProgress']))
+                inProgress.children[1].appendChild(article);
+            },
+            finish: (article) => {
+                complete.children[1].appendChild(article);
+            },
+            delete: (article) => {
+                article.remove();
+            }
+        }
+
+        return functions;
+    }
+
+    function generateArticle(elements, data, buttons) {
+        let articleEl = document.createElement('article');
+
+        for (let i = 0; i < elements.length; i++) {
+            let cell = document.createElement(elements[i]);
+            cell.textContent = data[i];
+            articleEl.appendChild(cell);
+        }
+        articleEl.appendChild(generateButtons(buttons['open']));
+        return articleEl;
+    }
+
+    function generateButtons(obj) {
+        let buttonD = document.createElement("div");
+        buttonD.setAttribute('class', 'flex');
+
+        for (const attr in obj) {
+            let button = document.createElement('button');
+            button.setAttribute('class', attr);
+            button.textContent = obj[attr];
+            buttonD.appendChild(button);
+        }
+        return buttonD;
     }
 }
