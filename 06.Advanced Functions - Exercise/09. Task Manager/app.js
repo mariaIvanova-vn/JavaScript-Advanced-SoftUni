bc0ca2bce8ee833cdec0dcfa2b087ee535797129
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-11 14:38:50 +0300

    add

--- 06.Advanced Functions - Exercise/09. Task Manager/app.js
+++ 06.Advanced Functions - Exercise/09. Task Manager/app.js
@@ -1,3 +1,16 @@
 function solve() {
-    console.log("//TODO")
+    document.getElementsByTagName('form')[0].addEventListener('submit', createTask);
+    let sections = document.getElementsByTagName('section');
+    let openSection = sections[1];
+    let inProgressSection = sections[2];
+    let completeSections = sections[3];
+
+    function createTask(event) {
+        event.preventDefault();
+        let form = event.target;
+        let task = form.elements[0].value;
+        let description = form.elements[1].value;
+        let date = form.elements[2].value;
+        debugger
+    }
 }
