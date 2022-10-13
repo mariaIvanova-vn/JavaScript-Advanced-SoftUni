e1501d4ed9e309080527de40ae970f09331b5543
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-13 13:49:14 +0300

    add

---
+++ 07.Unit Testing and Error Handling - Lab/tests/04. Sum of Numbers.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const { sum } = require('../04. Sum of Numbers');
+
+describe('Sum of Numbers', () => {
+    it('return sum of the values of all elements ', () => {
+        expect(sum([1, 2, 3])).to.be.equal(6);
+    });
+
+    it('should return false', () => {
+        expect(sum([1,1])).not.be.equal(7).false;
+    });
+
+    it('should return false, if input is not an array', () => {
+        expect(sum('')).is.not.Arguments(Array);
+    });
+})
