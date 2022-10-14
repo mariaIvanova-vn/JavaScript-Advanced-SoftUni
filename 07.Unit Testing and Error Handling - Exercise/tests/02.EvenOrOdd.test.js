bf553ec2e8bafadb18ca1a17d68796dadaba3807
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-14 10:20:38 +0300

    even or odd

--- 07.Unit Testing and Error Handling - Exercise/tests/02.EvenOrOdd.test.js
+++ 07.Unit Testing and Error Handling - Exercise/tests/02.EvenOrOdd.test.js
@@ -4,25 +4,22 @@ const { expect } = require('chai');
 describe('If invalid input', () => {
 
     it('should return undefined if input is number', () => {
-        expect(isOddOrEven(5)).to.be.equal(undefined);
+        expect(isOddOrEven(1)).to.be.equal(undefined);
     })
 
     it('should return undefined if input is object', () => {
-        expect(isOddOrEven({ name: 'Pesho' })).to.be.equal(undefined);
+        expect(isOddOrEven({ name: 'Maria' })).to.be.equal(undefined);
     })
 })
 
 describe('If valid input', () => {
 
     it('should return even if string has even number of symbols', () => {
-        expect(isOddOrEven('Marina')).to.be.equal('even');
+        expect(isOddOrEven('Dada')).to.be.equal('even');
     })
 
     it('should return odd if string has odd number of symbols', () => {
-        expect(isOddOrEven('Pesho')).to.be.equal('odd');
-    })
-
-    it('should should work correctly with sentences', () => {
-        expect(isOddOrEven('Pesho loves JavaScript')).to.be.equal('even');
+        expect(isOddOrEven('Dadad')).to.be.equal('odd');
+        expect(isOddOrEven('I am here!')).to.be.equal('even');
     })
 })
