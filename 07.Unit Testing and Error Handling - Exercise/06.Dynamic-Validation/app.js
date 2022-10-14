3d1dc4de19120b683277981fa7b52e6ab83102ba
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-14 14:21:31 +0300

    add

--- 07.Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ 07.Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+    let email=document.getElementById('email');
+    let regex=new RegExp('^[a-z]+@[a-z]+\\.[a-z]+');
+
+    email.addEventListener('change', ()=>{
+        if(regex.test(email.value)){
+            email.classList.remove('error');
+        }else{
+            email.classList.add('error');
+        }
+    })
 }
