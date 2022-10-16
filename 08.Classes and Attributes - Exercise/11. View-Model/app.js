9c45f1f2c36270b842af92ae8886c0b71ddad41d
mariaIvanova-vn <maria_vn86@abv.bg>
2022-10-16 21:51:08 +0300

    add 11

--- 08.Classes and Attributes - Exercise/11. View-Model/app.js
+++ 08.Classes and Attributes - Exercise/11. View-Model/app.js
@@ -1,8 +1,27 @@
 class Textbox {
-    //TODO
+    constructor(selector, regex) {
+        this._value = '';
+        this._elements = Array.from(document.querySelectorAll(selector));
+        this._invalidSymbol = regex;
+    }
+    get value() {
+        return this._value;
+    }
+    set value(v) {
+        this._value = v;
+        for (let i = 0; i < this._elements.length; i++) {
+            this._elements[i].value = v;
+        }
+    }
+    get elements(){
+        return this._elements;
+    }
+    isValid(){
+        return !this._invalidSymbol.test(this.value);
+    }
 }
 
-let textbox = new Textbox(".textbox",/[^a-zA-Z0-9]/);
+let textbox = new Textbox(".textbox", /[^a-zA-Z0-9]/);
 let inputs = document.getElementsByClassName('.textbox');
 
-inputs.addEventListener('click',function(){console.log(textbox.value);});
+inputs.addEventListener('click', function () { console.log(textbox.value); });
